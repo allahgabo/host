fbbe2b50d39b5d4da0f09e7a564b06fa0c4f3c17
suliman allahgabo <suliman_allahgabo@yahoo.com>
2020-06-20 23:14:50 +0530

    second commit

--- Curd_Pro/Curd_Pro/settings.py
+++ Curd_Pro/Curd_Pro/settings.py
@@ -75,12 +75,18 @@ WSGI_APPLICATION = 'Curd_Pro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.mysql',
+        #'NAME': 'curd',
+        #'USER':'root',
+        #'PASSWORD':'root',
+    #}
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'curd',
-        'USER':'root',
-        'PASSWORD':'root',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
